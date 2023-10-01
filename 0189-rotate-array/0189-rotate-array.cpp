9d4fa539f31414f78813213b06db7d6cf4a25085
Yash Shah <33934375+code-yash@users.noreply.github.com>
2023-10-01 16:11:54 +0530

    Time: 19 ms (84.02%), Space: 25.3 MB (49.3%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,22 +2,33 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
 
-        if(k >= nums.size())
-            k = k % nums.size();
+        
+        // k = k % nums.size();
 
-        queue<int> q;
+        // queue<int> q;
+        // int n = nums.size();
+        // for(int i=n-k; i<n; i++){
+        //     q.push(nums[i]);
+        // }
+
+        // for(int i = n-k-1; i>=0; i--){
+        //     nums[i+k] = nums[i];
+        // }
+
+        // for(int i=0; i<k; i++){
+        //     nums[i] = q.front();
+        //     q.pop();
+        // }
+
+
+        //-------------In O(1) Space Complexity----------------
+
+        k = k % nums.size();
         int n = nums.size();
-        for(int i=n-k; i<n; i++){
-            q.push(nums[i]);
-        }
-
-        for(int i = n-k-1; i>=0; i--){
-            nums[i+k] = nums[i];
-        }
-
-        for(int i=0; i<k; i++){
-            nums[i] = q.front();
-            q.pop();
+        if(k != 0){
+            reverse(nums.begin(), nums.begin() + (n-k));
+            reverse(nums.begin() + (n-k), nums.end());
+            reverse(nums.begin(), nums.end());
         }
         
     }
