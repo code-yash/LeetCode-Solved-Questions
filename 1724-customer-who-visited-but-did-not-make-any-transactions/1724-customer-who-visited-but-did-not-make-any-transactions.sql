a0fb36e01b80d6c8945c6232819f6666879395f1
Yash Shah <33934375+code-yash@users.noreply.github.com>
2023-10-05 02:03:29 +0530

    Time: 2314 ms (93.68%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,15 @@
 # Write your MySQL query statement below
 
-SELECT customer_id, count(*) as count_no_trans
-FROM (SELECT customer_id FROM Visits v LEFT JOIN Transactions t
-ON v.visit_id = t.visit_id
-WHERE t.visit_id IS NULL) as temp
-GROUP BY temp.customer_id;
+# SELECT customer_id, count(*) as count_no_trans
+# FROM (SELECT customer_id FROM Visits v LEFT JOIN Transactions t
+# ON v.visit_id = t.visit_id
+# WHERE t.visit_id IS NULL) as temp
+# GROUP BY temp.customer_id;
+
+
+/* Write your PL/SQL query statement below */
+SELECT customer_id, count(*) AS count_no_trans 
+FROM Visits  
+WHERE visit_id NOT IN (
+  SELECT visit_id FROM Transactions) 
+GROUP BY customer_id;
