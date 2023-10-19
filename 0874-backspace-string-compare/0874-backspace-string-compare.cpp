42080eea93ec1d3a559219bc8aef3c42f554a8b8
Yash Shah <33934375+code-yash@users.noreply.github.com>
2023-10-19 20:04:17 +0530

    Time: 0 ms (100%), Space: 6.6 MB (54.78%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -26,6 +26,7 @@ public:
     }
 
 
+
     bool usingStack(string s, string t){
         string s1 = removeHash(s);
         string s2 = removeHash(t);
@@ -36,8 +37,39 @@ public:
     }
 
 
+    //---------- Method 2 Using Pointers ----------------
+    string removeHash2(string s){
+        int curr = -1;
+
+        int n = s.length();
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '#'){
+                curr--;
+                if(curr < -1)
+                    curr = -1;
+            }
+            else{
+                curr++;
+                s[curr] = s[i];
+            }
+        }
+
+        s.resize(curr+1);
+        return s;
+
+    }
+
+
 
     bool backspaceCompare(string s, string t) {
-        return usingStack(s, t);      
+        // return usingStack(s, t);  
+
+        //Using 2 pointer technique
+
+        string s1 = removeHash2(s);
+        string s2 = removeHash2(t);
+
+        return (s1 == s2);    
     }
 };
